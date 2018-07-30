2d5facaab4983c08d7eb534cee0bd2dc6462ab44
tekstrand <thomas.ekstrand@bigcommerce.com>
2018-07-30 12:01:06 -0500

    test

---
+++ LinkitPlugin.php
@@ -0,0 +1,26 @@
+<?php
+namespace Craft;
+
+class LinkitPlugin extends BasePlugin
+{
+    public function getName()
+    {
+        return 'Linkit';
+    }
+
+    public function getVersion()
+    {
+        return '0.9.1.BC';
+    }
+
+    public function getDeveloper()
+    {
+        return 'Fruit Studios/Bigcommerce Fork';
+    }
+
+    public function getDeveloperUrl()
+    {
+        return 'https://github.com/lwilkowskeBC/Linkit';
+    }
+
+}
